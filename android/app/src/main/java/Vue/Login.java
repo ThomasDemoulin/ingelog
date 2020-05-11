a1b2bf88bfe4b67e8a16b4948a71e2a63c8e3169
thomas <thomasdemoulin08@gmail.com>
2020-05-11 15:20:41 +0200

    interaction première vue

--- android/app/src/main/java/Vue/Login.java
+++ android/app/src/main/java/Vue/Login.java
@@ -1,7 +1,10 @@
 package Vue;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.*;
 import android.widget.Button;
 
 public class Login extends AppCompatActivity {
@@ -15,6 +18,14 @@ public class Login extends AppCompatActivity {
 
         //Association de chaque élément avec la vue
         login_connexion = (Button) findViewById(R.id.login_connexion);
+
+        //Création des listeners
+        login_connexion.setOnClickListener( new OnClickListener() {
+            public void onClick(View v) {
+                Intent intent=new Intent().setClass(Login.this, Menu.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
--- android/app/src/main/java/Vue/Menu.java
+++ android/app/src/main/java/Vue/Menu.java
@@ -1,4 +1,32 @@
 package Vue;
 
-public class Menu {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Menu extends AppCompatActivity {
+
+    Button menu_robot;
+    Button menu_commandes;
+    Button menu_bogues;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        //Création de la vue sur l'application Android
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+
+        //Association de chaque élément avec la vue
+        menu_robot = (Button) findViewById(R.id.menu_robot);
+        menu_commandes = (Button) findViewById(R.id.menu_commandes);
+
+        //Création des listeners
+        menu_robot.setOnClickListener( new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent=new Intent().setClass(Menu.this, Connect.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
