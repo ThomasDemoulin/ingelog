a77679f65eb253d05c08b0825328c75ffdd0cc59
thomas <thomasdemoulin08@gmail.com>
2020-06-03 10:27:08 +0200

    historique des commandes

--- android/app/src/main/java/Vue/Bogues.java
+++ android/app/src/main/java/Vue/Bogues.java
@@ -3,12 +3,10 @@ package Vue;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.io.IOException;
 
